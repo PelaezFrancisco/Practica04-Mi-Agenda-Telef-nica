0d7b6b7bce7729040a2fdb1a99280862585b4642
PelaezFrancisco <pelaez.francisco@gmail.com>
2020-05-24 13:14:47 -0500

    Carpetas

---
+++ javascript/test.js
@@ -0,0 +1,8 @@
+alert('Test1');
+
+function funcion1(){
+  document.all.mensaje.outerHTML="<h3>Holiiiiiiiiiiiiiiii</h3>";
+  var elemento = document.createElement("h2");
+  elemento.inner
+  appendChild('body');
+}
